c661d6ab05f04564d502fe5cb2b9cf9255a7ef7a
Dmitriy Karpov <dmitriy_karpov@epam.com>
2022-12-20 19:34:02 +0300

    added exceptions

---
+++ src/main/java/by/karpov/test_clevertec/exception/DuplicateEntityException.java
@@ -0,0 +1,9 @@
+package by.karpov.test_clevertec.exception;
+
+public class DuplicateEntityException extends RuntimeException {
+
+    public DuplicateEntityException(String message) {
+        super(message);
+    }
+
+}
